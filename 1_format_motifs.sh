3c074e7fb128b136a9d35c377e852e11ee7759ac
Amanda Charbonneau <charbo24@msu.edu>
2019-10-29 22:19:37 -0400

    Create 1_format_motifs.sh

---
+++ 1_format_motifs.sh
@@ -0,0 +1,14 @@
+
+module load MEME/5.0.4
+
+for i in *.txt; do tail -n +2  ${i} | cut -f 2,3,4,5 | matrix2meme -dna > ${i}.fixed; done
+
+find ./ -size  0 -print0 |xargs -0 rm --
+
+for i in *.fixed; do newname=$(basename ${i} _2.00.txt.fixed); sed -i "s/MOTIF 1/${newname}/" ${i}; done
+
+justone=`ls *.fixed | head -n 1`
+
+head -9 ${justone} > allmotifs
+
+for i in *.fixed; do tail -n +9 ${i} >> allmotifs; done
