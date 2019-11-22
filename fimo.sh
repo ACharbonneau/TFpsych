88fce4fe5fc959cb67de7316703b8dc5a8a0aeab
Amanda Charbonneau <charbo24@msu.edu>
2019-11-22 07:44:41 -0500

    Delete fimo.sh

--- fimo.sh
+++
@@ -1,7 +0,0 @@
-
-bedtools getfasta -fi ../RawData/hg19.fa -bed pgc.scz2_regions.bed > pgc.scz2_regions.fasta
-
-bedtools getfasta -name -tab -fi ../RawData/hg19.fa -bed pgc.scz2_regions.bed > pgc.scz2_regions.tsv
-
-
-fimo ../pwms_all_motifs/allmotifs pgc.scz2_regions.fasta
